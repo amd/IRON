628f47e563fd70d55ff941de1ac6f48ee37ad836
andrej <an.roesti@gmail.com>
2025-11-12 22:26:51 +0000

    acquire github runner registration token outside of runner

--- ci/docker-based/inside_docker/actions_runner.sh
+++ ci/docker-based/inside_docker/actions_runner.sh
@@ -21,27 +21,6 @@ install_runner() {
   rm runner.tar.gz
 }
 
-get_registration_token() {
-  if [ "${SCOPE}" = "repo" ]; then
-    URL="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/actions/runners/registration-token"
-  elif [ "${SCOPE}" = "org" ]; then
-    URL="https://api.github.com/orgs/${GITHUB_OWNER}/actions/runners/registration-token"
-  else
-    echo "Unknown scope: ${SCOPE}"
-    exit 1
-  fi
-
-  TOKEN=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" \
-    -H "Accept: application/vnd.github+json" "${URL}" \
-    | jq -r .token)
-
-  if [ "${TOKEN}" = "null" ]; then
-    echo "Failed to get runner registration token"
-    exit 1
-  fi
-  echo "${TOKEN}"
-}
-
 configure_runner() {
     local token="$1"
     cd "${RUNNER_DIR}"
@@ -56,13 +35,8 @@ configure_runner() {
 }
 
 install_runner
-if ! token="$(get_registration_token)"; then
-  echo "Failed to get runner registration token" >&2
-  exit 1
-fi
-export token
-echo "Got runner token: ${token}"
-configure_runner "${token}"
+
+configure_runner "${GITHUB_RUNNER_TOKEN}"
 
 echo "Configured. Running actions runner..."
 cd "${RUNNER_DIR}"
