282a721245bfbf4cb5f38cfe8944be4219c8de5e
andrej <an.roesti@gmail.com>
2025-11-13 09:23:07 -0700

    include updated operators (runlist memcpy, mha updates, softmax, swiglu)
    Co-authored-by: cubansil <CurtJohn.Bansil@amd.com>
    Co-authored-by: Victor Jung <victor.jung@amd.com>
    Co-authored-by: pvasireddy-amd <pvasired@amd.com>
    Co-authored-by: André Rösti <an.roesti@gmail.com>

--- operators/softmax/softmax.py
+++ operators/softmax/softmax.py
@@ -87,8 +87,7 @@ def softmax(dev, num_elements, num_columns, num_channels, trace_size, tile_size)
 
     # Runtime operations to move data to/from the AIE-array
     rt = Runtime()
-    dummy_ty = tensor_ty
-    with rt.sequence(tensor_ty, dummy_ty, tensor_ty) as (A, dummy, C):
+    with rt.sequence(tensor_ty, tensor_ty) as (A, C):
         rt.start(*my_workers)
 
         # Initialize a group for parallel drain tasks, with fill resources free'd when drains complete.
