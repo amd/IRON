1e0348f0fbedf6b98b130cf936473c3d2d82fb3f
kurtis-b-1 <cubansil@amd.com>
2025-11-14 13:56:08 -0700

    Fix AIE2 compilation

--- aie_kernels/aie2/aie2_math.h
+++ aie_kernels/aie2/aie2_math.h
@@ -18,6 +18,7 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <cstring>
 
 // fast inverse square root implementation from Quake III Arena
 inline __attribute__((always_inline)) float invsqrt(float in)
@@ -25,7 +26,7 @@ inline __attribute__((always_inline)) float invsqrt(float in)
     float x2 = in * 0.5f;
     float y = in;
     int32_t i;
-    memcpy(&i, &y, sizeof(y)); // avoid strict-aliasing
+    std::memcpy(&i, &y, sizeof(y)); // avoid strict-aliasing
     i = 0x5f3759df - (i >> 1);
     y = *(float *)&i;
     const float threehalfs = 1.5f;
