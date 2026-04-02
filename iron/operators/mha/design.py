cdfe9a6eb978dcc334de76e09d91a10e6d648ac6
Erika Hunhoff <erika.hunhoff@amd.com>
2026-04-02 12:53:30 -0600

    small fixes

--- iron/operators/mha/design.py
+++ iron/operators/mha/design.py
@@ -894,7 +894,3 @@ def fused_mha(
     # Place components (assign them resources on the device) and generate an MLIR module
     module = my_program.resolve_program(SequentialPlacer())
     return module
-
-
-if __name__ == "__main__":
-    main()
